6744810280f0d0f95fcfb7388c2c6c1489db6f16
EduAir94 <49965850+EduAir94@users.noreply.github.com>
2022-09-15 19:57:41 -0300

    fix: measure performance

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let no_prime_text = [];
 
 document.getElementById("nums").addEventListener("keyup", function (event) {
+  const s = performance.now();
   const val = event.target.value;
   let arrNumeros = val
     .split(",")
     .filter((el) => el)
-    .map((num) => parseInt(num));
+    .map((num) => Math.abs(parseInt(num)));
   arrNumeros = arrNumeros.filter((x, i) => arrNumeros.indexOf(x) === i);
 
   let arrPrime = buscarPrimos(arrNumeros);
@@ -14,10 +15,24 @@ document.getElementById("nums").addEventListener("keyup", function (event) {
     .map((el) => `<li class="list-group-item">${el}</li>`)
     .join("");
   document.getElementById("no_prime").innerHTML = arr;
+  const dis = (performance.now() - s).toFixed(3);
+  document.getElementById("performance").innerHTML = dis;
 });
 
 function esPrimo(num) {
-  for (let i = 2; i < num; i++) {
+  if (num < 2) {
+    no_prime_text.push(`${num} no es primo porque es menor a 2`);
+    return false;
+  }
+  if (num % 2 == 0) {
+    no_prime_text.push(`${num} no es primo porque es divisible entre 2`);
+    return false;
+  }
+  if (num % 3 == 0) {
+    no_prime_text.push(`${num} no es primo porque es divisible entre 3`);
+    return false;
+  }
+  for (let i = 5; i < num / 2; i += 2) {
     if (num % i === 0) {
       no_prime_text.push(`${num} no es primo porque es divisible entre ${i}`);
       return false;
